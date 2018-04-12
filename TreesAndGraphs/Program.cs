0de9619c67b49998c8748e6cc7e08af1972b286d
Prithvi Pal Singh <Prithvi.Coer@outlook.com>
2018-04-12 15:11:48 +0800

    Understanding Hash functions

--- TreesAndGraphs/Program.cs
+++ TreesAndGraphs/Program.cs
@@ -15,27 +15,27 @@ namespace TreesAndGraphs
         static void Main(string[] args)
         {
             BinarySearchTree bs = new BinarySearchTree();
-            bs.Put(18, "S"); //6
-            bs.Put(5, "E"); //2
-            bs.Put(3, "A"); //0
+            bs.Put(18, "S"); //6            
+            bs.Put(5, "E"); //2            
+            bs.Put(3, "A"); //0            
             bs.Put(15, "R"); ///5
             bs.Put(4, "C"); //1
             bs.Put(9, "H"); //3
             bs.Put(23, "X"); //7
             bs.Put(10, "M"); //4
-           // bs.Put(12, "P"); //5
+            //bs.Put(12, "P"); //5
             //bs.Put(6, "L");
             //Console.WriteLine(bs.Size());
             //Console.WriteLine(bs.Rank(5));//2
             //Console.WriteLine(bs.Rank(19));
-            Console.WriteLine(bs.RangeCount(5, 18));
-            bs.RangeValues(5, 18);
-            Console.WriteLine(bs.RangeCount(5, 19));
-            bs.RangeValues(5, 19);
-            Console.WriteLine(bs.RangeCount(6, 18));
-            bs.RangeValues(6, 18);
-            Console.WriteLine(bs.RangeCount(6, 19));
-            bs.RangeValues(6, 19);
+            //Console.WriteLine(bs.RangeCount(5, 18));
+            //bs.RangeValues(5, 18);
+            //Console.WriteLine(bs.RangeCount(5, 19));
+            //bs.RangeValues(5, 19);
+            //Console.WriteLine(bs.RangeCount(6, 18));
+            //bs.RangeValues(6, 18);
+            //Console.WriteLine(bs.RangeCount(6, 19));
+            //bs.RangeValues(6, 19);
             //var a = bs.Iterator();
             //foreach (var item in a)
             //{
@@ -72,8 +72,25 @@ namespace TreesAndGraphs
             //Console.WriteLine(rbs.Rank(5));
             //#endregion
 
-
-
+            #region - Hash Functions Understanding
+            string str1 = "polygenelubricant";
+            int count = 0;
+            for (int i = 0; i < 10000; i++)
+            {
+                str1 = str1 + i;
+                //var abc = str1.GetHashCode() % 8;
+                //var abc = Math.Abs(str1.GetHashCode()) % 8;
+                var abc = (str1.GetHashCode() & 0X7fffffff) % 8;
+                if (abc < 0)
+                {
+                    count++;
+                }
+            }
+            //Console.WriteLine(Math.Abs(str1.GetHashCode()));
+            //Console.WriteLine(str1.GetHashCode() & 0X7fffffff);
+            //Console.WriteLine(-2 & 0X7fffffff);
+            Console.WriteLine("Done: " + count);
+            #endregion
             Console.Read();
         }
     }
