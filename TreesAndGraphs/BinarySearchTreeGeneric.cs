6fd4f0a959c78abaa343c1e349c86a1ee8ef11d6
Prithvi Pal Singh <Prithvi.Coer@outlook.com>
2018-05-02 16:40:39 +0800

    1. Segment tree. 2. Implementing generic BST

---
+++ TreesAndGraphs/BinarySearchTreeGeneric.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreesAndGraphs
+{
+    /// <summary>
+    /// 1. Left node key is smaller than key of root node and right node key is 
+    ///    greater than key of root node.
+    /// 2. For red black tree:
+    ///    2.1 Larger key of 2-3 tree as root
+    ///    2.2 Red link glues nodes within a 3-node.
+    ///    2.3 Black link connects 2-nodes and 3-nodes
+    ///    2.4 No node has two red link connected to it.
+    ///    2.5 Every path from root to null link contains same number of black links
+    ///    2.6 All red links lean left
+    ///    2.7 Search for RB-BST is same as BST same is the case with Ceiling, Selection
+    /// 3. Count- Number of nodes under a certain node x (including x)
+    /// 4. Rank- Number of nodes that have keys less than certain node with that key.
+    /// </summary>
+    class BinarySearchTreeGeneric<TKey, TValue> where TKey : IComparable<TKey>
+    {
+        private GenericNode Root;
+
+        private class GenericNode
+        {
+            private TKey key;
+            private TValue value;
+            private GenericNode left;
+            private GenericNode right;
+            private int count;
+
+            public GenericNode(TKey key, TValue value)
+            {
+                this.Key = key;
+                this.Value = value;
+            }
+
+            public TKey Key { get => key; set => key = value; }
+            public TValue Value { get => value; set => this.value = value; }
+            public GenericNode Right { get => right; set => right = value; }
+            public GenericNode Left { get => left; set => left = value; }
+            public int Count { get => count; set => count = value; }
+        }
+
+        /// <summary>
+        /// Insret item into a BST
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public void Put(TKey key, TValue value)
+        {
+            Root = Put(Root, key, value);
+        }
+
+        /// <summary>
+        /// Fetch an item from BST
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public TValue Get(TKey key)
+        {
+            GenericNode x = Root;
+            while (x != null)
+            {
+                int cmp = key.CompareTo(x.Key);
+                if (cmp < 0)
+                {
+                    x = x.Left;
+                }
+                else if (cmp > 0)
+                {
+                    x = x.Right;
+                }
+                else
+                {
+                    return x.Value;
+                }
+            }
+
+            return default(TValue);
+        }
+
+        /// <summary>
+        /// Delete minimum i.e. leftmost item from BST.
+        /// </summary>
+        public void DeleteMinimum()
+        {
+            DeleteMinimum(Root);
+        }
+
+        /// <summary>
+        /// Hibbard deletion - delete any item
+        /// </summary>
+        /// <param name="key"></param>
+        public void Delete(TKey key)
+        {
+            Root = Delete(key, Root);
+        }
+
+        /// <summary>
+        /// Find minimum in BST
+        /// </summary>
+        /// <returns></returns>
+        public TKey Min()
+        {
+            var min = Min(Root);
+            return min.Key;
+        }
+
+        /// <summary>
+        /// Get floor value in BST i.e. immediate lower value
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public TKey Floor(TKey key)
+        {
+            GenericNode x = Floor(Root, key);
+            if (x == null)
+                return default(TKey);
+
+            return x.Key;
+        }
+
+        /// <summary>
+        /// Get ceiling value i.e. immediate upper value
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public TKey Ceiling(TKey key)
+        {
+            GenericNode x = Ceiling(Root, key);
+            if (x == null)
+                return default(TKey);
+
+            return x.Key;
+        }
+
+        /// <summary>
+        /// form an iterator for BST
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<TKey> Iterator()
+        {
+            Queue<TKey> q = new Queue<TKey>();
+            InOrder(Root, q);
+            return q;
+        }
+
+        /// <summary>
+        /// Get size of BST
+        /// </summary>
+        /// <returns></returns>
+        public int Size()
+        {
+            return Size(Root);
+        }
+
+        /// <summary>
+        /// Get rank of a node
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public int Rank(TKey key)
+        {
+            return Rank(key, Root);
+        }
+
+        //public int RangeCount(TKey low, TKey high)
+        //{
+        //    return !string.IsNullOrWhiteSpace(Get(high)) ? 1 + Rank(high) - Rank(low) : Rank(high) - Rank(low);
+        //}
+
+        //public void RangeValues(TKey low, TKey high)
+        //{
+        //    Queue<TKey> q = new Queue<TKey>();
+        //    var a = Iterator();
+        //    foreach (var item in a)
+        //    {
+        //        if (item >= low && item <= high)
+        //        {
+        //            q.Enqueue(item);
+        //        }
+        //    }
+
+        //    foreach (var item in q)
+        //    {
+        //        Console.Write(Get(item) + " ");
+        //    }
+
+        //    Console.WriteLine();
+        //}
+
+        ////TODO: Understand count
+        private GenericNode Put(GenericNode node, TKey key, TValue value)
+        {
+            if (node == null)
+            {
+                var a = new GenericNode(key, value);
+                a.Count = 1;
+                return a;
+            }
+
+            int cmp = key.CompareTo(node.Key);
+            if (cmp < 0)
+                node.Left = Put(node.Left, key, value);
+            else if (cmp > 0)
+                node.Right = Put(node.Right, key, value);
+            else
+                node.Value = value;
+
+            node.Count = 1 + Size(node.Left) + Size(node.Right);
+
+            return node;
+        }
+
+        private void InOrder(GenericNode x, Queue<TKey> q)
+        {
+            if (x == null)
+                return;
+
+            InOrder(x.Left, q);
+            q.Enqueue(x.Key);
+            InOrder(x.Right, q);
+        }
+
+        private GenericNode Floor(GenericNode x, TKey key)
+        {
+            if (x == null)
+                return null;
+
+            int cmp = key.CompareTo(x.Key);
+
+            if (cmp == 0)
+                return x;
+
+            if (cmp < 0)
+                return Floor(x.Left, key);
+
+            GenericNode t = Floor(x.Right, key);
+            if (t != null)
+                return t;
+            else
+                return x;
+        }
+
+        private GenericNode Ceiling(GenericNode x, TKey key)
+        {
+            if (x == null)
+                return null;
+
+            int cmp = key.CompareTo(x.Key);
+            if (cmp == 0)
+                return x;
+
+            if (cmp > 0)
+            {
+                return Ceiling(x.Right, key);
+            }
+
+            GenericNode t = Ceiling(x.Left, key);
+            if (t != null)
+                return t;
+            else
+                return x;
+        }
+
+        private int Size(GenericNode x)
+        {
+            if (x == null)
+                return 0;
+
+            return x.Count;
+        }
+
+        private int Rank(TKey key, GenericNode x)
+        {
+            if (x == null)
+                return 0;
+
+            int cmp = key.CompareTo(x.Key);
+
+            if (cmp < 0)
+                return Rank(key, x.Left);
+            else if (cmp > 0)
+                return 1 + Size(x.Left) + Rank(key, x.Right);
+            else
+                return Size(x.Left);
+        }
+
+        private GenericNode DeleteMinimum(GenericNode x)
+        {
+            if (x.Left == null)
+                return x.Right;
+
+            x.Left = DeleteMinimum(x.Left);
+            x.Count = 1 + Size(x.Left) + Size(x.Right);
+            return x;
+        }
+
+        private GenericNode Delete(TKey key, GenericNode x)
+        {
+            if (x == null)
+                return null;
+
+            int cmp = key.CompareTo(x.Key);
+
+            //Search for key
+            if (cmp < 0)
+            {
+                x.Left = Delete(key, x.Left);
+            }
+            else if (cmp > 0)
+            {
+                x.Right = Delete(key, x.Right);
+            }
+            else
+            {
+                if (x.Right == null)
+                    return x.Left;
+
+                GenericNode t = x;
+                x = Min(t.Right);
+                x.Right = DeleteMinimum(t.Right);
+                x.Left = t.Left;
+            }
+
+            x.Count = 1 + Size(x.Left) + Size(x.Right);
+            return x;
+        }
+
+        private GenericNode Min(GenericNode x)
+        {
+            if (x == null)
+                return null;
+
+            while (x.Left != null)
+            {
+                x = x.Left;
+            }
+
+            return x;
+        }
+
+    }
+}
--- TreesAndGraphs/Program.cs
+++ TreesAndGraphs/Program.cs
@@ -14,6 +14,18 @@ namespace TreesAndGraphs
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            TestBSTGeneric();
+            Console.Read();
+        }
+
+        static void TestFileIndexing()
+        {
+            FileIndexing f = new FileIndexing();
+            f.Indexing();
+        }
+
+        static void TestBinarySearchTree()
+        {
             BinarySearchTree bs = new BinarySearchTree();
             bs.Put(18, "S"); //6            
             bs.Put(5, "E"); //2            
@@ -23,24 +35,24 @@ namespace TreesAndGraphs
             bs.Put(9, "H"); //3
             bs.Put(23, "X"); //7
             bs.Put(10, "M"); //4
-            //bs.Put(12, "P"); //5
-            //bs.Put(6, "L");
-            //Console.WriteLine(bs.Size());
-            //Console.WriteLine(bs.Rank(5));//2
-            //Console.WriteLine(bs.Rank(19));
-            //Console.WriteLine(bs.RangeCount(5, 18));
-            //bs.RangeValues(5, 18);
-            //Console.WriteLine(bs.RangeCount(5, 19));
-            //bs.RangeValues(5, 19);
-            //Console.WriteLine(bs.RangeCount(6, 18));
-            //bs.RangeValues(6, 18);
-            //Console.WriteLine(bs.RangeCount(6, 19));
-            //bs.RangeValues(6, 19);
-            //var a = bs.Iterator();
-            //foreach (var item in a)
-            //{
-            //    Console.WriteLine(bs.Get(item));
-            //}
+                             //bs.Put(12, "P"); //5
+                             //bs.Put(6, "L");
+                             //Console.WriteLine(bs.Size());
+                             //Console.WriteLine(bs.Rank(5));//2
+                             //Console.WriteLine(bs.Rank(19));
+                             //Console.WriteLine(bs.RangeCount(5, 18));
+                             //bs.RangeValues(5, 18);
+                             //Console.WriteLine(bs.RangeCount(5, 19));
+                             //bs.RangeValues(5, 19);
+                             //Console.WriteLine(bs.RangeCount(6, 18));
+                             //bs.RangeValues(6, 18);
+                             //Console.WriteLine(bs.RangeCount(6, 19));
+                             //bs.RangeValues(6, 19);
+                             //var a = bs.Iterator();
+                             //foreach (var item in a)
+                             //{
+                             //    Console.WriteLine(bs.Get(item));
+                             //}
 
             //Console.WriteLine(bs.Get(5));
             //bs.DeleteMinimum();
@@ -48,89 +60,113 @@ namespace TreesAndGraphs
             //bs.DeleteMinimum();
             //Console.WriteLine((bs.Min()).Value);
             //Console.WriteLine(bs.Get(10));
+        }
 
-            //#region -- RBT test area
-            //RedBlackSearchTree rbs = new RedBlackSearchTree();
-            //rbs.Put(18, "S");
-            //rbs.Put(5, "E");
-            //rbs.Put(3, "A");
-            //rbs.Put(15, "R");
-            //rbs.Put(4, "C");
-            //rbs.Put(9, "H");
-            //rbs.Put(23, "X");
-            //rbs.Put(10, "M");
-            //rbs.Put(12, "P");
-            //rbs.Put(6, "L");
+        static void TestRedBlackTree()
+        {
+            #region -- RBT test area
+            RedBlackSearchTree rbs = new RedBlackSearchTree();
+            rbs.Put(18, "S");
+            rbs.Put(5, "E");
+            rbs.Put(3, "A");
+            rbs.Put(15, "R");
+            rbs.Put(4, "C");
+            rbs.Put(9, "H");
+            rbs.Put(23, "X");
+            rbs.Put(10, "M");
+            rbs.Put(12, "P");
+            rbs.Put(6, "L");
 
-            ////var a = rbs.Iterator();
-            ////foreach (var item in a)
-            ////{
-            ////    Console.WriteLine(rbs.Get(item));
-            ////}
-
-            //Console.WriteLine(rbs.Size());
-            //Console.WriteLine(rbs.Rank(5));
-            //#endregion
-
-            #region - Hash Functions Understanding
-            //string str1 = "polygenelubricant";
-            //int count = 0;
-            //for (int i = 0; i < 10000; i++)
+            //var a = rbs.Iterator();
+            //foreach (var item in a)
             //{
-            //    str1 = str1 + i;
-            //    //var abc = str1.GetHashCode() % 8;
-            //    //var abc = Math.Abs(str1.GetHashCode()) % 8;
-            //    var abc = (str1.GetHashCode() & 0X7fffffff) % 8;
-            //    if (abc < 0)
-            //    {
-            //        count++;
-            //    }
+            //    Console.WriteLine(rbs.Get(item));
             //}
-            //Console.WriteLine(Math.Abs(str1.GetHashCode()));
-            //Console.WriteLine(str1.GetHashCode() & 0X7fffffff);
-            //Console.WriteLine(-2 & 0X7fffffff);
-            //Console.WriteLine("Done: " + count);
 
-            //Console.WriteLine("Aa".GetHashCode());
-            //Console.WriteLine("BB".GetHashCode());
+            Console.WriteLine(rbs.Size());
+            Console.WriteLine(rbs.Rank(5));
             #endregion
+        }
 
-            //SeperateChainingHash<string, string> sc = new SeperateChainingHash<string, string>();
-            //sc.Put("S", "S");
-            //sc.Put("E", "E");
-            //sc.Put("A", "A");
-            //sc.Put("R", "R");
-            //sc.Put("C", "C");
-            //sc.Put("H", "H");
-            //sc.Put("E", "E");
-            //sc.Put("X", "X");
-            //sc.Put("A", "A");
-            //sc.Put("M", "M");
-            //sc.Put("P", "P");
-            //sc.Put("L", "L");
-            //sc.Put("E", "E");
-            //Console.WriteLine(sc.Get("E"));    
+        static void TestHashFunctions()
+        {
+            #region - Hash Functions Understanding
+            string str1 = "polygenelubricant";
+            int count = 0;
+            for (int i = 0; i < 10000; i++)
+            {
+                str1 = str1 + i;
+                //var abc = str1.GetHashCode() % 8;
+                //var abc = Math.Abs(str1.GetHashCode()) % 8;
+                var abc = (str1.GetHashCode() & 0X7fffffff) % 8;
+                if (abc < 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(Math.Abs(str1.GetHashCode()));
+            Console.WriteLine(str1.GetHashCode() & 0X7fffffff);
+            Console.WriteLine(-2 & 0X7fffffff);
+            Console.WriteLine("Done: " + count);
 
-            //LinearProbingHash<string, string> lph = new LinearProbingHash<string, string>();
-            //lph.Put("S", "S");
-            //lph.Put("E", "E");
-            //lph.Put("A", "A");
-            //lph.Put("R", "R");
-            //lph.Put("C", "C");
-            //lph.Put("H", "H");
-            //lph.Put("E", "E");
-            //lph.Put("X", "X");
-            //lph.Put("A", "A");
-            //lph.Put("M", "M");
-            //lph.Put("P", "P");
-            //lph.Put("L", "L");
-            //lph.Put("E", "E");
-            //Console.WriteLine(lph.Get("P"));
+            Console.WriteLine("Aa".GetHashCode());
+            Console.WriteLine("BB".GetHashCode());
+            #endregion
+        }
 
-            //FileIndexing f = new FileIndexing();
-            //f.Indexing();
+        static void TestSeperateChaining()
+        {
+            SeperateChainingHash<string, string> sc = new SeperateChainingHash<string, string>();
+            sc.Put("S", "S");
+            sc.Put("E", "E");
+            sc.Put("A", "A");
+            sc.Put("R", "R");
+            sc.Put("C", "C");
+            sc.Put("H", "H");
+            sc.Put("E", "E");
+            sc.Put("X", "X");
+            sc.Put("A", "A");
+            sc.Put("M", "M");
+            sc.Put("P", "P");
+            sc.Put("L", "L");
+            sc.Put("E", "E");
+            Console.WriteLine(sc.Get("E"));
+        }
 
-            ////Sparse vector implementation
+        static void TestLinearProbing()
+        {
+            LinearProbingHash<string, string> lph = new LinearProbingHash<string, string>();
+            lph.Put("S", "S");
+            lph.Put("E", "E");
+            lph.Put("A", "A");
+            lph.Put("R", "R");
+            lph.Put("C", "C");
+            lph.Put("H", "H");
+            lph.Put("E", "E");
+            lph.Put("X", "X");
+            lph.Put("A", "A");
+            lph.Put("M", "M");
+            lph.Put("P", "P");
+            lph.Put("L", "L");
+            lph.Put("E", "E");
+            Console.WriteLine(lph.Get("P"));
+        }
+        static void TestSegmentTree()
+        {
+            int[] arr = { 1, 3, 5, 7, 9, 11 };
+            int n = arr.Length;
+            SegmentTree st = new SegmentTree();
+            var stArray = st.constructST(arr, arr.Length);
+            Console.WriteLine(string.Join(" ", stArray));
+            Console.WriteLine(st.getSum(stArray, n, 0, 2));
+            st.updateValue(arr, stArray, n, 2, 10);
+            Console.WriteLine(st.getSum(stArray, n, 0, 2));
+            Console.Read();
+        }
+
+        ////Sparse vector implementation
+        static void TestSparseVector()
+        {
             SparseVector[] st = new SparseVector[5];
             st[0] = new SparseVector();
             st[1] = new SparseVector();
@@ -158,8 +194,57 @@ namespace TreesAndGraphs
             {
                 Console.WriteLine(item);
             }
+        }
 
-            Console.Read();
+        static void TestBSTGeneric()
+        {
+            BinarySearchTreeGeneric<string, string> bs = new BinarySearchTreeGeneric<string, string>();
+            bs.Put("S", "S"); //6            
+            bs.Put("E", "E"); //2            
+            bs.Put("A", "A"); //0            
+            bs.Put("R", "R"); ///5
+            bs.Put("C", "C"); //1
+            bs.Put("H", "H"); //3
+            bs.Put("E", "E"); //7
+            bs.Put("X", "X"); //4
+            bs.Put("A", "A"); //6            
+            bs.Put("M", "M"); //2            
+            bs.Put("P", "P"); //0            
+            bs.Put("L", "L"); ///5
+            bs.Put("E", "E"); //1
+            var a = bs.Iterator();
+            foreach (var item in a)
+            {
+                Console.Write(bs.Get(item) + " ");
+            }
+
+            // bs.Delete("S");
+            Console.WriteLine();
+            //a = bs.Iterator();
+            //foreach (var item in a)
+            //{
+            //    Console.Write(bs.Get(item) + " ");
+            //}
+            //Console.WriteLine(bs.Floor("T"));
+            //Console.WriteLine(bs.Ceiling("T"));
+
+            //Console.WriteLine(bs.Size());
+            //bs.Delete("S");
+            //Console.WriteLine(bs.Size());
+
+            //Console.WriteLine(bs.Rank("A"));
+            //Console.WriteLine(bs.Rank("C"));
+            //Console.WriteLine(bs.Rank("E"));
+            //Console.WriteLine(bs.Rank("S"));
+            //Console.WriteLine(bs.Rank("X"));
+            //bs.Delete("E");
+            //Console.WriteLine(bs.Rank("A"));
+            //Console.WriteLine(bs.Rank("C"));
+            //Console.WriteLine(bs.Rank("E"));
+            //Console.WriteLine(bs.Rank("S"));
+            //Console.WriteLine(bs.Rank("X"));
+
+            Console.WriteLine(bs.Min());
         }
     }
 }
